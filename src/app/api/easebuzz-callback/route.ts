be37366097480ce922f797ef6cc3c1f3dd277c36
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-12-19 13:55:44 +0530

    hi

--- src/app/api/easebuzz-callback/route.ts
+++ src/app/api/easebuzz-callback/route.ts
@@ -38,7 +38,7 @@ export async function POST(req: Request) {
     console.log("🧾 Final Detected Payment Status:", paymentstatus);
 
     // ✅ Base URL safety (detect if env missing)
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL?.trim() || "https://psmservices.org/";
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL?.trim() || "http://localhost:3000/";
 
     // console.log("🌐 Using base URL:", baseUrl);
     // console.log("📦 Payment Status:", paymentstatus);
